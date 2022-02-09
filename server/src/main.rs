75fc08df3943ff788cf47197f88f2f8b14d204c5
Steven Casper <sebastiancasper3@gmail.com>
2022-02-09 04:01:30 -0500

    Beginning of rewrite

---
+++ server/src/main.rs
@@ -0,0 +1,63 @@
+use clash::protocol::{Connection, Message};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::spawn;
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:42932").await.unwrap();
+
+    loop {
+        let (socket, port) = listener.accept().await.unwrap();
+        println!("{socket:?} \n{port:?}");
+
+        spawn(async move { handle_new_connection(socket).await });
+    }
+}
+
+async fn handle_new_connection(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    // Generate an auth id for this user
+    // TODO
+    let auth_id = 1;
+    connection
+        .write_frame(Message::ConnectionAccept { auth_id })
+        .await;
+
+    loop {
+        let incoming = connection.read_frame().await;
+
+        match incoming {
+            Message::GameHost { auth_id, lobby_id } => todo!(),
+            Message::GameJoin { auth_id, lobby_id } => todo!(),
+            Message::GameLobbyInfo { auth_id, lobby_id } => todo!(),
+            Message::GameBegin { auth_id, lobby_id } => todo!(),
+            Message::GameEnd { auth_id, lobby_id } => todo!(),
+            Message::GameLeave { auth_id, lobby_id } => todo!(),
+            Message::GameOptions {
+                auth_id,
+                lobby_id,
+                options,
+            } => {
+                todo!()
+            }
+            Message::GameCurrentRoom {
+                auth_id,
+                lobby_id,
+                room,
+            } => todo!(),
+            Message::GameItemCollected {
+                auth_id,
+                lobby_id,
+                item,
+            } => {
+                todo!()
+            }
+            m => {
+                println!("Client sent a server only message: {m:?}")
+            }
+        }
+    }
+}
---
+++ src/protocol.rs
@@ -0,0 +1,86 @@
+use tokio::{io::AsyncReadExt, io::AsyncWriteExt, net::TcpStream};
+
+// TODO: Take more advantage of the type system (e.g. Client/Server messages)
+#[derive(Debug)]
+pub enum Message {
+    ConnectionAccept {
+        auth_id: u32,
+    },
+    GameHost {
+        auth_id: u32,
+        lobby_id: u32,
+    },
+    GameJoin {
+        auth_id: u32,
+        lobby_id: u32,
+    },
+    GameOptions {
+        auth_id: u32,
+        lobby_id: u32,
+        options: Options,
+    },
+    GameLobbyInfo {
+        auth_id: u32,
+        lobby_id: u32,
+    },
+    GameBegin {
+        auth_id: u32,
+        lobby_id: u32,
+    },
+    GameCurrentRoom {
+        auth_id: u32,
+        lobby_id: u32,
+        room: Room,
+    },
+    GameForceWarp {
+        auth_id: u32,
+        lobby_id: u32,
+        room: Room,
+    },
+    GameItemCollected {
+        auth_id: u32,
+        lobby_id: u32,
+        item: Item,
+    },
+    GameEnd {
+        auth_id: u32,
+        lobby_id: u32,
+    },
+    GameLeave {
+        auth_id: u32,
+        lobby_id: u32,
+    },
+}
+
+#[derive(Debug)]
+pub enum Item {
+    Spatula,
+    Fuse,
+}
+
+#[derive(Debug)]
+pub enum Room {}
+
+#[derive(Debug)]
+pub struct Options {
+    pub max_spats: u8,
+    pub ng_plus: bool,
+}
+
+pub struct Connection {
+    socket: TcpStream,
+}
+
+impl Connection {
+    pub fn new(socket: TcpStream) -> Self {
+        Self { socket }
+    }
+
+    pub async fn read_frame(&mut self) -> Message {
+        todo!()
+    }
+
+    pub async fn write_frame(&mut self, frame: Message) {
+        todo!()
+    }
+}
