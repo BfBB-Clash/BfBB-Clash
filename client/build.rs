5d1ae52660ecf3a12851cec94788d4bf9173733f
Steven Casper <sebastiancasper3@gmail.com>
2022-03-26 22:36:25 -0400

    Set App Icon

---
+++ client/build.rs
@@ -0,0 +1,9 @@
+use winres::WindowsResource;
+
+fn main() -> std::io::Result<()> {
+    #[cfg(windows)]
+    {
+        WindowsResource::new().set_icon("res/icon.ico").compile()?;
+    }
+    Ok(())
+}
