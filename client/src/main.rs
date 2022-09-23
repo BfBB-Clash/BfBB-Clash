d9f156559651f0ec69389f023dff7ffa197ed3e0
Steven Casper <sebastiancasper3@gmail.com>
2022-09-23 12:02:01 -0700

    Enable windows console for debug builds

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(
-    all(target_os = "windows", not(feature = "console")),
+    all(target_os = "windows", not(debug_assertions), not(feature = "console")),
     windows_subsystem = "windows"
 )]
 
