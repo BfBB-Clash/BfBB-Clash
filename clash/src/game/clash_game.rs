e091c8d042923b1744ff4ff009e3faee5f9c6a14
Steven Casper <sebastiancasper3@gmail.com>
2022-10-14 23:03:11 -0700

    Make ClashGame stateful

--- clash/src/game/clash_game.rs
+++ clash/src/game/clash_game.rs
@@ -1,7 +1,8 @@
 use std::collections::HashSet;
 
-use bfbb::game_interface::game_var::{GameVar, InterfaceBackend};
-use bfbb::game_interface::{GameInterface, InterfaceError, InterfaceResult};
+use bfbb::game_interface::dolphin::DolphinInterface;
+use bfbb::game_interface::game_var::{GameVar, GameVarMut};
+use bfbb::game_interface::{InterfaceError, InterfaceProvider, InterfaceResult};
 use bfbb::game_state::{GameMode as BfBBGameMode, GameOstrich};
 use bfbb::{IntoEnumIterator, Level, Spatula};
 use clash_lib::lobby::{GamePhase, NetworkedLobby};
@@ -9,116 +10,170 @@ use clash_lib::net::{Item, LobbyMessage, Message};
 use clash_lib::PlayerId;
 use log::info;
 
+use crate::gui::GuiSender;
 use crate::net::{NetCommand, NetCommandSender};
 
 use super::game_mode::GameMode;
 
-pub struct ClashGame;
+pub struct ClashGame {
+    provider: DolphinInterface,
+    lobby: NetworkedLobby,
+    /// Not sure if this is the best approach, the idea is that it would be faster
+    /// to store a local state of what we as a client have collected rather
+    /// than searching to see if we've collected it.
+    local_spat_state: HashSet<Spatula>,
+    player_id: PlayerId,
+}
+
+impl ClashGame {
+    pub fn new(player_id: PlayerId) -> Self {
+        Self {
+            provider: DolphinInterface::default(),
+            // FIXME: would be better to be able to construct this object after receiving the initial lobby from the sever
+            // but that would complicate the logic thread logic a lot since the initial lobby is received as a regular LobbyMessage::GameLobbyInfo
+            lobby: NetworkedLobby::new(0),
+            local_spat_state: HashSet::new(),
+            player_id,
+        }
+    }
+}
 
 impl GameMode for ClashGame {
     /// Process state updates from the server and report back any actions of the local player
-    fn update<F: InterfaceBackend>(
-        &mut self,
-        interface: &mut GameInterface<F>,
-        lobby: &mut NetworkedLobby,
-        local_player: PlayerId,
-        network_sender: &mut NetCommandSender,
-        local_spat_state: &mut HashSet<Spatula>,
-    ) -> InterfaceResult<()> {
-        if interface.is_loading.get()? {
-            return Ok(());
-        }
-
-        // TODO: Use a better error
-        // Find the local player within the lobby
-        let local_player = lobby
-            .players
-            .get_mut(&local_player)
-            .ok_or(InterfaceError::Other)?;
-
-        // Detect level changes
-        let level = Some(interface.get_current_level()?);
-        if local_player.current_level != level {
-            local_player.current_level = level;
-            network_sender
-                .try_send(NetCommand::Send(Message::Lobby(
-                    LobbyMessage::GameCurrentLevel { level },
-                )))
-                .unwrap();
-        }
-
-        // We need to be on the title screen to start a new game
-        // but we can't if the Demo FMV is active, so we also check that we're currently in a scene.
-        let can_start = interface.game_mode.get()? == BfBBGameMode::Title
-            && interface.game_ostrich.get()? == GameOstrich::InScene;
-        if local_player.ready_to_start != can_start {
-            local_player.ready_to_start = can_start;
-            network_sender
-                .try_send(NetCommand::Send(Message::Lobby(
-                    LobbyMessage::PlayerCanStart(can_start),
-                )))
-                .unwrap();
-        }
-
-        // Don't proceed if the game is not active
-        if lobby.game_phase != GamePhase::Playing || level == Some(Level::MainMenu) {
-            return Ok(());
-        }
-
-        // Set the cost to unlock the lab door
-        interface.set_lab_door(
-            lobby.options.lab_door_cost.into(),
-            local_player.current_level,
-        )?;
-
-        // Check for newly collected spatulas
-        for spat in Spatula::iter() {
-            // Skip already collected spatulas
-            if local_spat_state.contains(&spat) {
-                interface.mark_task_complete(spat)?;
-                interface.collect_spatula(spat, local_player.current_level)?;
-                continue;
+    fn update(&mut self, network_sender: &NetCommandSender) -> InterfaceResult<()> {
+        self.provider.do_with_interface(|interface| {
+            if interface.is_loading.get()? {
+                return Ok(());
             }
 
-            if let Some(spat_ref) = lobby.game_state.spatulas.get_mut(&spat) {
-                if spat_ref.collection_vec.len() != lobby.options.tier_count.into() {
-                    interface.unlock_task(spat)?;
-                } else {
-                    // Sync collected spatulas
-                    interface.collect_spatula(spat, local_player.current_level)?;
+            // TODO: Use a better error
+            // Find the local player within the lobby
+            let local_player = self
+                .lobby
+                .players
+                .get_mut(&self.player_id)
+                .ok_or(InterfaceError::Other)?;
+
+            // Detect level changes
+            let level = Some(interface.get_current_level()?);
+            if local_player.current_level != level {
+                local_player.current_level = level;
+                network_sender
+                    .try_send(NetCommand::Send(Message::Lobby(
+                        LobbyMessage::GameCurrentLevel { level },
+                    )))
+                    .unwrap();
+            }
+
+            // We need to be on the title screen to start a new game
+            // but we can't if the Demo FMV is active, so we also check that we're currently in a scene.
+            let can_start = interface.game_mode.get()? == BfBBGameMode::Title
+                && interface.game_ostrich.get()? == GameOstrich::InScene;
+            if local_player.ready_to_start != can_start {
+                local_player.ready_to_start = can_start;
+                network_sender
+                    .try_send(NetCommand::Send(Message::Lobby(
+                        LobbyMessage::PlayerCanStart(can_start),
+                    )))
+                    .unwrap();
+            }
+
+            // Don't proceed if the game is not active
+            if self.lobby.game_phase != GamePhase::Playing || level == Some(Level::MainMenu) {
+                return Ok(());
+            }
+
+            // Set the cost to unlock the lab door
+            interface.set_lab_door(
+                self.lobby.options.lab_door_cost.into(),
+                local_player.current_level,
+            )?;
+
+            // Check for newly collected spatulas
+            for spat in Spatula::iter() {
+                // Skip already collected spatulas
+                if self.local_spat_state.contains(&spat) {
                     interface.mark_task_complete(spat)?;
+                    interface.collect_spatula(spat, local_player.current_level)?;
                     continue;
+                }
+
+                if let Some(spat_ref) = self.lobby.game_state.spatulas.get_mut(&spat) {
+                    if spat_ref.collection_vec.len() != self.lobby.options.tier_count.into() {
+                        interface.unlock_task(spat)?;
+                    } else {
+                        // Sync collected spatulas
+                        interface.collect_spatula(spat, local_player.current_level)?;
+                        interface.mark_task_complete(spat)?;
+                        continue;
+                    }
+                }
+
+                // Check menu for any potentially missed collection events
+                // This is the only way to detect Kah-Rah-Tae and Small Shall Rule
+                if interface.is_task_complete(spat)? {
+                    self.local_spat_state.insert(spat);
+                    network_sender
+                        .try_send(NetCommand::Send(Message::Lobby(
+                            LobbyMessage::GameItemCollected {
+                                item: Item::Spatula(spat),
+                            },
+                        )))
+                        .unwrap();
+                    info!("Collected (from menu) {spat:?}");
+                }
+
+                // Detect spatula collection events
+                if interface.is_spatula_being_collected(spat, local_player.current_level)? {
+                    self.local_spat_state.insert(spat);
+                    network_sender
+                        .try_send(NetCommand::Send(Message::Lobby(
+                            LobbyMessage::GameItemCollected {
+                                item: Item::Spatula(spat),
+                            },
+                        )))
+                        .unwrap();
+                    info!("Collected {spat:?}");
                 }
             }
 
-            // Check menu for any potentially missed collection events
-            // This is the only way to detect Kah-Rah-Tae and Small Shall Rule
-            if interface.is_task_complete(spat)? {
-                local_spat_state.insert(spat);
-                network_sender
-                    .try_send(NetCommand::Send(Message::Lobby(
-                        LobbyMessage::GameItemCollected {
-                            item: Item::Spatula(spat),
-                        },
-                    )))
-                    .unwrap();
-                info!("Collected (from menu) {spat:?}");
-            }
+            Ok(())
+        })
+    }
 
-            // Detect spatula collection events
-            if interface.is_spatula_being_collected(spat, local_player.current_level)? {
-                local_spat_state.insert(spat);
-                network_sender
-                    .try_send(NetCommand::Send(Message::Lobby(
-                        LobbyMessage::GameItemCollected {
-                            item: Item::Spatula(spat),
-                        },
-                    )))
-                    .unwrap();
-                info!("Collected {spat:?}");
+    fn message(&mut self, message: LobbyMessage, gui_sender: &mut GuiSender) {
+        match message {
+            LobbyMessage::GameBegin => {
+                self.local_spat_state.clear();
+                let lobby = &mut self.lobby;
+
+                let _ = self.provider.do_with_interface(|i| {
+                    i.powers.start_with_powers(lobby.options.ng_plus)?;
+                    i.start_new_game()
+                });
+                gui_sender
+                    .send((self.player_id, lobby.clone()))
+                    .expect("GUI has crashed and so will we");
             }
+            LobbyMessage::GameLobbyInfo { lobby: new_lobby } => {
+                // This could fail if the user is restarting dolphin, but that will desync a lot of other things as well
+                // so it's fine to just wait for a future lobby update to correct the issue
+                let _ = self.provider.do_with_interface(|i| {
+                    i.spatula_count
+                        .set(new_lobby.game_state.spatulas.len() as u32)
+                });
+                self.lobby = new_lobby.clone();
+                gui_sender
+                    .send((self.player_id, new_lobby))
+                    .expect("GUI has crashed and so will we");
+            }
+            // We're not yet doing partial updates
+            LobbyMessage::PlayerOptions { options: _ } => todo!(),
+            LobbyMessage::GameOptions { options: _ } => todo!(),
+            LobbyMessage::GameEnd => todo!(),
+            LobbyMessage::PlayerCanStart(_) => todo!(),
+            LobbyMessage::GameCurrentLevel { level: _ } => todo!(),
+            LobbyMessage::GameItemCollected { item: _ } => todo!(),
         }
-
-        Ok(())
     }
 }
--- clash/src/game/mod.rs
+++ clash/src/game/mod.rs
@@ -1,19 +1,16 @@
 mod clash_game;
 mod game_mode;
 
-use bfbb::game_interface::game_var::GameVarMut;
-use bfbb::game_interface::{dolphin::Dolphin, InterfaceError};
-use bfbb::{EnumCount, Spatula};
+use bfbb::game_interface::InterfaceError;
 use clash_lib::net::LobbyMessage;
 use clash_lib::{lobby::NetworkedLobby, net::Message, PlayerId};
 use eframe::egui::Context;
 use spin_sleep::LoopHelper;
-use std::collections::HashSet;
 use std::sync::mpsc::{Receiver, Sender};
 use tokio::sync::oneshot::error::TryRecvError;
 
-use crate::gui::GuiSender;
-use crate::net::{NetCommand, NetCommandSender};
+use crate::net::NetCommandSender;
+use crate::{gui::GuiSender, net::NetCommand};
 
 use self::{clash_game::ClashGame, game_mode::GameMode};
 
@@ -31,34 +28,19 @@ pub fn start_game(
         .report_interval_s(0.5)
         .build_with_target_rate(126);
 
-    // TODO: Report hooking errors to user/stdout
-    let interface = Dolphin::default();
-    let mut game = ClashGame;
-    // TODO: Avoid starting this thread until we have our player_id, then change our receiver to
-    // take `LobbyMessage`s
-    let player_id = 0;
-    let lobby = None;
-    // Not sure if this is the best approach, the idea is that it would be faster
-    // to store a local state of what we as a client have collected rather
-    // than searching to see if we've collected it.
-    let local_spat_state = HashSet::<Spatula>::with_capacity(Spatula::COUNT);
-
     let mut logic = Logic {
         gui_ctx,
         gui_sender,
-        dol: interface,
         network_sender,
         logic_receiver,
-        lobby,
-        player_id,
-        local_spat_state,
+        game: None,
     };
 
     while let Err(TryRecvError::Empty) = shutdown_receiver.try_recv() {
         loop_helper.loop_start();
 
         logic.update_from_network().unwrap();
-        logic.update(&mut game);
+        logic.update();
 
         loop_helper.loop_start_s();
     }
@@ -67,98 +49,49 @@ pub fn start_game(
 struct Logic {
     gui_ctx: Context,
     gui_sender: GuiSender,
-    // TODO: probably be generic over a new `GameInterfaceProvider`
-    dol: Dolphin,
     network_sender: NetCommandSender,
     logic_receiver: Receiver<Message>,
-    lobby: Option<NetworkedLobby>,
-    player_id: PlayerId,
-    local_spat_state: HashSet<Spatula>,
+    game: Option<ClashGame>,
 }
 
 impl Logic {
-    fn update<G: GameMode>(&mut self, game: &mut G) {
-        let lobby = match self.lobby.as_mut() {
+    fn update(&mut self) {
+        let game = match self.game.as_mut() {
             Some(it) => it,
             _ => return,
         };
-
-        let res = self.dol.with_interface(|i| {
-            game.update(
-                i,
-                lobby,
-                self.player_id,
-                &mut self.network_sender,
-                &mut self.local_spat_state,
-            )
-        });
-        if let Err(InterfaceError::Unhooked) = res {
+        // TODO: Log non-hooking errors
+        if let Err(InterfaceError::Unhooked) = game.update(&self.network_sender) {
             // We lost dolphin
-            if let Some(local_player) = lobby.players.get_mut(&self.player_id) {
-                if local_player.current_level != None {
-                    local_player.current_level = None;
-                    self.network_sender
-                        .try_send(NetCommand::Send(Message::Lobby(
-                            LobbyMessage::GameCurrentLevel { level: None },
-                        )))
-                        .unwrap();
-                    self.network_sender
-                        .try_send(NetCommand::Send(Message::Lobby(
-                            LobbyMessage::PlayerCanStart(false),
-                        )))
-                        .unwrap();
-                }
-            }
+            // Our local state will be updated when the client accepts this message and responds.
+            self.network_sender
+                .try_send(NetCommand::Send(Message::Lobby(
+                    LobbyMessage::GameCurrentLevel { level: None },
+                )))
+                .unwrap();
+            self.network_sender
+                .try_send(NetCommand::Send(Message::Lobby(
+                    LobbyMessage::PlayerCanStart(false),
+                )))
+                .unwrap();
         }
     }
 
     fn update_from_network(&mut self) -> Result<(), InterfaceError> {
         for msg in self.logic_receiver.try_iter() {
             let action = match msg {
-                Message::ConnectionAccept { player_id: id } => {
-                    self.player_id = id;
+                Message::ConnectionAccept { player_id } => {
+                    self.game = Some(ClashGame::new(player_id));
                     continue;
                 }
                 Message::Lobby(m) => m,
                 _ => continue,
             };
 
-            match action {
-                LobbyMessage::GameBegin => {
-                    self.local_spat_state.clear();
-                    let _ = self.dol.with_interface(|i| i.start_new_game());
-                    let lobby = self
-                        .lobby
-                        .as_mut()
-                        .expect("Tried to begin game without being in a lobby");
-
-                    let _ = self
-                        .dol
-                        .with_interface(|i| i.powers.start_with_powers(lobby.options.ng_plus));
-                    self.gui_sender
-                        .send((self.player_id, lobby.clone()))
-                        .expect("GUI has crashed and so will we");
-                }
-                LobbyMessage::GameLobbyInfo { lobby: new_lobby } => {
-                    // This could fail if the user is restarting dolphin, but that will desync a lot of other things as well
-                    // so it's fine to just wait for a future lobby update to correct the issue
-                    let _ = self.dol.with_interface(|i| {
-                        i.spatula_count
-                            .set(new_lobby.game_state.spatulas.len() as u32)
-                    });
-                    self.lobby = Some(new_lobby.clone());
-                    self.gui_sender
-                        .send((self.player_id, new_lobby))
-                        .expect("GUI has crashed and so will we");
-                }
-                // We're not yet doing partial updates
-                LobbyMessage::PlayerOptions { options: _ } => todo!(),
-                LobbyMessage::GameOptions { options: _ } => todo!(),
-                LobbyMessage::GameEnd => todo!(),
-                LobbyMessage::PlayerCanStart(_) => todo!(),
-                LobbyMessage::GameCurrentLevel { level: _ } => todo!(),
-                LobbyMessage::GameItemCollected { item: _ } => todo!(),
-            }
+            self.game
+                .as_mut()
+                .expect("Tried to process a LobbyAction without having a gamemode setup")
+                .message(action, &mut self.gui_sender);
 
             // Signal the UI to update
             self.gui_ctx.request_repaint();
