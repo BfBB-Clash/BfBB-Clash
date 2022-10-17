d28c64600ba1bf93c2376cdbc08f0fd9db7569fe
Steven Casper <sebastiancasper3@gmail.com>
2022-10-16 22:15:58 -0400

    Bfbb 0.3 (#98)
    * Account for bfbblib changes
    
    * Make ClashGame stateful
    
    * Use bfbb main branch
    
    * Cargo Update
    
    * Use beta in CI
    
    * Fix new clippy lint
    
    * Move to bfbb crates.io release

--- clash/src/game/game_mode.rs
+++ clash/src/game/game_mode.rs
@@ -1,23 +1,13 @@
-use std::collections::HashSet;
+use bfbb::game_interface::InterfaceResult;
+use clash_lib::net::LobbyMessage;
 
-use bfbb::{
-    game_interface::{GameInterface, InterfaceResult},
-    Spatula,
-};
-use clash_lib::{lobby::NetworkedLobby, PlayerId};
-
-use crate::net::NetCommandSender;
+use crate::{gui::GuiSender, net::NetCommandSender};
 
 // TODO: Revisit this when new unique game modes are created.
 //  Idea is to allow game mode logic to be implemented by an arbitrary
 //  struct with a consistent interface.
 pub trait GameMode {
-    fn update<G: GameInterface>(
-        &mut self,
-        interface: &G,
-        lobby: &mut NetworkedLobby,
-        local_player: PlayerId,
-        network_sender: &mut NetCommandSender,
-        local_spat_state: &mut HashSet<Spatula>,
-    ) -> InterfaceResult<()>;
+    fn update(&mut self, network_sender: &NetCommandSender) -> InterfaceResult<()>;
+
+    fn message(&mut self, message: LobbyMessage, gui_sender: &mut GuiSender);
 }
